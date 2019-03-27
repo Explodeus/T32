7bcb09037058574597e0223fb4c089b7c7feea3e
Explode <volodya.sokha@gmail.com>
2019-03-27 17:16:05 +0200

    editingAdded

--- T3/src/Dispatcher.java
+++ T3/src/Dispatcher.java
@@ -16,27 +16,35 @@ public class Dispatcher {
         }
         System.exit(0);                 //exit once app is done
     }
+
     private static boolean readFile() {
         System.out.println("Please enter your files name and path i.e C:\\test.txt: ");//prompt for file name
         filename = sc.nextLine();              //read in the file name
         Scanner fileToRead = null;
         try {
+
             fileToRead = new Scanner(new File(filename)); //point the scanner method to a file
-            //check if there is a next line and it is not null and then read it in
+                                                          //check if there is a next line and it is not null and then read it in
+
             for (String line; fileToRead.hasNextLine() && (line = fileToRead.nextLine()) != null; ) {
-                System.out.println(line);//print each line as its read
-                stringBufferOfData.append(line).append("\r\n");//this small line here is to appened all text read in from the file to a string buffer which will be used to edit the contents of the file
+                System.out.println(line);                 //print each line as its read
+
+                stringBufferOfData.append(line).append("\r\n");//appeneding all text read in from the file to a string buffer which will be used to edit the contents of the file
             }
+
             fileToRead.close();//this is used to release the scanner from file
             return true;
         } catch (FileNotFoundException ex) {//if the file cannot be found an exception will be thrown
             System.out.println("The file " + filename + " could not be found! " + ex.getMessage());
             return false;
-        } finally {//if an error occurs now we close the file to exit gracefully
+        } finally {                 //if an error occurs now we close the file to exit gracefully
             fileToRead.close();
             return true;
         }
-            /* The below is another way in which to read the file, however i think the shorter method would be the scanner class
+
+            /*----------------------------------------------------------------------------------------------------------------
+            ------------------------------------------------------------------------------------------------------------------
+             The below is another way in which to read the file, however i think the shorter method would be the scanner class
             The reason for this is the below commented method uses 5 extra imports:
             import java.io.BufferedReader;
             import java.io.DataInputStream;
@@ -55,7 +63,8 @@ public class Dispatcher {
             System.out.println("The file " + filename + " could not be found or opened! "+ex.getMessage());
             System.exit(0);
             }
-             */
+            -------------------------------------------------------------------------------------------------------------------
+            -------------------------------------------------------------------------------------------------------------------*/
     }
     private static void writeToFile() {
         try {
@@ -67,15 +76,28 @@ public class Dispatcher {
         }
     }
     private static void replacement() {
-        System.out.println("Please enter the contents of a line you would like to edit: ");//prompt for a line in file to edit
-        String lineToEdit = sc.nextLine();//read the line to edit
-        System.out.println("Please enter the the replacement text: ");//prompt for a line in file to replace
-        String replacementText = sc.nextLine();//read the line to replace
-        //System.out.println(sb);//used for debugging to check that my stringbuffer has correct contents and spacing
-        int startIndex = stringBufferOfData.indexOf(lineToEdit);//now we get the starting point of the text we want to edit
-        int endIndex = startIndex + lineToEdit.length();//now we add the staring index of the text with text length to get the end index
-        stringBufferOfData.replace(startIndex, endIndex, replacementText);//this is where the actual replacement of the text happens
-        System.out.println("Here is the new edited text:\n" + stringBufferOfData); //used to debug and check the string was replaced
+        System.out.println("Please enter the contents of a line you would like to do: ");
+        System.out.println("Print edit/new line print");
+
+        String whatToDo = sc.nextLine();//read the line to do smth
+        String lineToEdit = sc.nextLine();
+
+        if (whatToDo == "edit"){
+            System.out.println("Please enter the contents of a line you would like to edit:");
+            System.out.println("Please enter the the replacement text: ");//prompt for a line in file to replace
+            String replacementText = sc.nextLine();//read the line to replace
+            //System.out.println(sb);//used for debugging to check that my stringbuffer has correct contents and spacing
+            int startIndex = stringBufferOfData.indexOf(lineToEdit);//now we get the starting point of the text we want to edit
+            int endIndex = startIndex + lineToEdit.length();//now we add the staring index of the text with text length to get the end index
+            stringBufferOfData.replace(startIndex, endIndex, replacementText);//this is where the actual replacement of the text happens
+            System.out.println("Here is the new edited text:\n" + stringBufferOfData); //used to debug and check the string was replaced
+        }else if(whatToDo == "new line print" || whatToDo =="newlineprint"){
+            System.out.println("Please enter the new line text:");
+            //TODO
+        }
+
+
+
     }
 }
 
